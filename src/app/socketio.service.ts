4eb8510f007b1a077c47ca98c9fa280a383da401
sarahdelfino <sarahdelfino7@gmail.com>
2021-09-12 18:22:47 -0400

    firebase

--- src/app/socketio.service.ts
+++ src/app/socketio.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import * as io from 'socket.io-client';
-import { environment } from './environment';
+import { environment } from '../environments/environment';
 import { Game } from './game';
 import { nanoid } from "nanoid";
 import { Player } from './player';
