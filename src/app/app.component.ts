8450fb30c724cae1cae72fd92a77d211ca2a5f75
sarahdelfino <sarahdelfino7@gmail.com>
2021-09-12 20:54:16 -0400

    start page edits

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,27 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { GameService } from './game.service';
 import * as io from 'socket.io-client';
 import { SocketioService } from './socketio.service';
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBgjfSBidyvXk8khmILej32MC1iJu7x89I",
-  authDomain: "death-box-28ecf.firebaseapp.com",
-  projectId: "death-box-28ecf",
-  storageBucket: "death-box-28ecf.appspot.com",
-  messagingSenderId: "917048488305",
-  appId: "1:917048488305:web:5af3d89596a2d8deaf06d0",
-  measurementId: "G-RMWB0565QP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 @Component({
   selector: 'app-root',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,40 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 export const environment = {
   production: false,
-  SOCKET_ENDPOINT: 'http://localhost:3000'
+  SOCKET_ENDPOINT: 'http://localhost:3000',
+  firebase: {
+    apiKey: "AIzaSyBgjfSBidyvXk8khmILej32MC1iJu7x89I",
+    authDomain: "death-box-28ecf.firebaseapp.com",
+    projectId: "death-box-28ecf",
+    storageBucket: "death-box-28ecf.appspot.com",
+    messagingSenderId: "917048488305",
+    appId: "1:917048488305:web:5af3d89596a2d8deaf06d0",
+    measurementId: "G-RMWB0565QP"
+  }
 };
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBgjfSBidyvXk8khmILej32MC1iJu7x89I",
+//   authDomain: "death-box-28ecf.firebaseapp.com",
+//   projectId: "death-box-28ecf",
+//   storageBucket: "death-box-28ecf.appspot.com",
+//   messagingSenderId: "917048488305",
+
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 /*
  * For easier debugging in development mode, you can import the following file
