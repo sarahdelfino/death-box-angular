585b079ca7a7b966f55543d5396213777327541c
sarahdelfino <sarahdelfino7@gmail.com>
2022-04-04 14:19:36 -0400

    fixed layout colors and breakpoints

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { DatabaseService } from '../database.service';
+import { Game } from '../game';
+import { nanoid } from "nanoid";
 
 @Component({
   selector: 'app-login',
@@ -9,13 +13,35 @@ import { AuthService } from '../auth.service';
 })
 export class LoginComponent {
 
+  game: Game;
+  loggedIn: boolean;
 
   constructor(
     public authService: AuthService,
+    private router: Router,
+    private dbService: DatabaseService,
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if (this.authService.isLoggedIn) {
+      this.loggedIn = true;
+    } else {
+      this.loggedIn = false;
+    }
+  }
 
+  createGame() {
+    this.createGameId();
+    sessionStorage.setItem('host', 'true');
+    console.log(this.game);
+    console.log(sessionStorage.getItem('host'));
+    this.dbService.create(this.game);
+    this.router.navigateByUrl(`/lobby/${this.game.id}`);
+  }
+
+  createGameId() {
+    var id = nanoid(5);
+    this.game = new Game(id, false,);
   }
 
 }
