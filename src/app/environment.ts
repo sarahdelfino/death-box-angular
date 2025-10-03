825f185e5fc8ad6d57fcf3ca6ea1f4d07e1a61cb
Sarah Delfino <sarahdelfino7@gmail.com>
2025-10-03 14:27:51 -0400

    analytics

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -13,7 +13,7 @@ export const environment = {
     storageBucket: "death-box-28ecf.appspot.com",
     messagingSenderId: "917048488305",
     appId: "1:917048488305:web:5af3d89596a2d8deaf06d0",
-    measurementId: "G-RMWB0565QP"
+    measurementId: "G-CRCM73VNGM"
   }
 };
 
