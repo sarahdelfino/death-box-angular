3ff5bc2c5d3384670fdc022aba5c007536b0f8bf
Sarah Delfino <sarahdelfino7@gmail.com>
2026-01-05 10:39:24 -0500

    removed ssr from play

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -5,6 +5,10 @@ export const serverRoutes: ServerRoute[] = [
     path: 'lobby/:id',
     renderMode: RenderMode.Client,
   },
+    {
+    path: 'play/:id',
+    renderMode: RenderMode.Client,
+  },
   {
     path: '**',
     renderMode: RenderMode.Prerender
